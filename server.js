5a0cfc9fd6c02bf8f9e1f2a639ca6477c7c118b1
Kuba-Kowal <148563319+Kuba-Kowal@users.noreply.github.com>
2025-07-09 10:23:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 import express from 'express';
 import { WebSocketServer } from 'ws';
-import fs from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 import { tmpdir } from 'os';
 import { OpenAI } from 'openai';
